4589c8b5d39be01a2807c925ebab031c6c71dbae
Andres Pena <penchjpc@gmail.com>
2022-05-10 15:34:29 -0400

    added state to manage prompt and response, and added jsx to show response below textarea

--- src/App.js
+++ src/App.js
@@ -2,18 +2,22 @@ import Form from './components/Form'
 import Responses from './components/Responses'
 import './App.css';
 import React from 'react';
-const engine_id = "text-curie-001"
-const data = {
-    prompt: "Write a poem about a dog wearing skis",
-    temperature: 0.5,
-    max_tokens: 64,
-    top_p: 1.0,
-    frequency_penalty: 0.0,
-    presence_penalty: 0.0,
-}
 
 function App() {
-  const [ response, setResponse ] = React.useState( '' )
+  // state to manage input from the user
+  const [ userInput, setUserInput ] = React.useState( 'Write a poem about a dog wearing skis' )
+  // state to manage response from openAI API
+  const [ responses, setResponses ] = React.useState( [] )
+  const engine_id = "text-curie-001"
+  const data = {
+      prompt: `${userInput}`,
+      temperature: 0.5,
+      max_tokens: 64,
+      top_p: 1.0,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+  }
+
 
   React.useEffect( () => {
     fetch(`https://api.openai.com/v1/engines/${engine_id}/completions`, {
@@ -27,7 +31,7 @@ function App() {
     .then( res => res.json() )
     .then( answer => {
       console.log(answer.choices)
-      setResponse( answer.choices[0].text)
+      setResponses( answer.choices[0].text)
     })
   }, [])
   function handleChange(){
@@ -36,8 +40,8 @@ function App() {
   return (
     <div className="App">
       <h1> Fun with AI </h1>
-      <Form value={response} handler={handleChange}/>
-      <Responses/>
+      <Form value={userInput} handler={handleChange}/>
+      <Responses data={responses} input={userInput} />
     </div>
   );
 }
--- src/components/Responses.js
+++ src/components/Responses.js
@@ -1,7 +1,13 @@
 import React from 'react'
 
-export default function Responses() {
+export default function Responses(props) {
+  const resArr = props.data
   return (
-    <div>Responses</div>
+    <div className='response-card'>
+      <h4 className='response-card-prompt-heading'>prompt:</h4>
+      <p className='response-card-prompt-paragraph'> {props.input}</p>
+      <h4 className='response-card-response-heading'>response:</h4>
+      <p className='response-card-response-paragraph'>{resArr}</p>
+    </div>
   )
 }
