dc7b9dbe046199d9e42634d7896409229a22102a
Andres Pena <penchjpc@gmail.com>
2022-05-10 15:06:55 -0400

    successfully submitted propmt to openAI API with environment variable

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
       method: "POST",
       headers: {
           "Content-Type": "application/json",
-          Authorization: `Bearer ${process.env.OPENAI_API_KEY} `
+          Authorization: `Bearer ${process.env.REACT_APP_OPENAI_API_KEY} `
       },
       body: JSON.stringify(data),
     })
